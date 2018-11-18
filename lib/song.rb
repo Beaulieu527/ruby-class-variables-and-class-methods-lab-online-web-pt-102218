8cb4ab27927bf4e1fdec8192edd04204b9d63826
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-18 03:39:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   @@count = 0
   @@genres = []
   @@artists = []
-  attr_accessor :name, :artists, :genres
+  attr_accessor :name, :artist, :genre
 
   def initialize(self)
     @@count += 1
